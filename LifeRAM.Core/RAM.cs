3f1e782747077fdf5487949be8391145955ba146
Han Wang <wahan9501@gmail.com>
2023-12-25 23:51:35 -0800

    Initial version

---
+++ LifeRAM.Core/RAM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeRAM.Core;
+
+[Table("RAM")]
+public class RAM
+{
+    public long Id { get; init; }
+
+    public DateTimeOffset StartDateTime { get; set; }
+    
+    public TimeSpan Length { get; set; }
+}
+
+public class RAMDbContext : DbContext
+{
+    public DbSet<RAM>? RAM { get; set; }
+
+    public RAMDbContext(DbContextOptions<RAMDbContext> options) : base(options)
+    {
+    }
+}
